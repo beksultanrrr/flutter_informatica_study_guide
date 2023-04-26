6ca202111deb1a45c0a236d714795a95be947410
beksultanrrr <78639334+beksultanrrr@users.noreply.github.com>
2023-04-26 20:11:43 +0600

    Update studentCard.dart

--- lib/screen/studentCard.dart
+++ lib/screen/studentCard.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class IdCard extends StatefulWidget {
+  const IdCard({super.key});
+
   @override
   _IdCardState createState() => _IdCardState();
 }
@@ -13,100 +15,101 @@ class _IdCardState extends State<IdCard> {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text("ID Card"),
+        title: const Text("ID Card"),
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
       ),
-  
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage("assets/studentImg.png"),
-                radius: 40.0,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage("assets/studentImg.png"),
+                  radius: 40.0,
+                ),
               ),
-            ),
-            Divider(
-              height: 30.0,
-              color: Colors.grey[700],
-            ),
-            Text(
-              "NAME",
-              style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              "Samet Rauan Maratqyzy",
-              style: TextStyle(
-                  color: Colors.amber,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text(
-              "Faculty",
-              style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              "FIT",
-              style: TextStyle(
-                  color: Colors.amber,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text(
-              "Cource",
-              style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              "3",
-              style: TextStyle(
-                  color: Colors.amber,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  "rauan.samet@icloud.com",
-                  style: TextStyle(
-                      color: Colors.grey[400],
-                      fontSize: 18.0,
-                      letterSpacing: 1.0),
-                )
-              ],
-            )
-          ],
+              Divider(
+                height: 30.0,
+                color: Colors.grey[700],
+              ),
+              const Text(
+                "NAME",
+                style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              const Text(
+                "Samet Rauan Maratqyzy",
+                style: TextStyle(
+                    color: Colors.amber,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              const Text(
+                "Faculty",
+                style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              const Text(
+                "FIT",
+                style: TextStyle(
+                    color: Colors.amber,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              const Text(
+                "Cource",
+                style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              const Text(
+                "3",
+                style: TextStyle(
+                    color: Colors.amber,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400],
+                  ),
+                  const SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    "rauan.samet@icloud.com",
+                    style: TextStyle(
+                        color: Colors.grey[400],
+                        fontSize: 18.0,
+                        letterSpacing: 1.0),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
